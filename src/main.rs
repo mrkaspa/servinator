5e797c095d65f9c7224419e72e2b30222c60cf3e
michel perez <michel.ingesoft@gmail.com>
2017-06-16 20:43:11 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 extern crate serde;
 extern crate serde_json;
+extern crate csv;
 
 #[macro_use]
 extern crate serde_derive;
--- src/runner.rs
+++ src/runner.rs
@@ -3,13 +3,24 @@ use serde_json;
 use std::fs::File;
 use std::path::Path;
 use std::io::prelude::*;
-use std::io::Error;
+use std::io;
+use csv::Reader;
+use csv;
 
 #[derive(Deserialize)]
 #[derive(Debug)]
 struct Config {
     token: String,
     customer_id: String,
+}
+
+
+#[derive(Debug,Deserialize)]
+struct Record {
+    city: String,
+    region: String,
+    country: String,
+    population: Option<u64>,
 }
 
 pub fn run(data: &Data) {
@@ -19,7 +30,7 @@ pub fn run(data: &Data) {
     };
 }
 
-fn load_config(data: &Data) -> Result<Config, Error> {
+fn load_config(data: &Data) -> Result<Config, io::Error> {
     let config_file = match data.config {
         Some(ref config_file) => config_file.clone(),
         None => String::from("config.json"),
@@ -29,7 +40,7 @@ fn load_config(data: &Data) -> Result<Config, Error> {
     Ok(config)
 }
 
-fn load_config_from_file(file_path: &String) -> Result<Config, Error> {
+fn load_config_from_file(file_path: &String) -> Result<Config, io::Error> {
     let path = Path::new(file_path);
     let mut file = File::open(&path).expect("File not found");
     let mut buf = String::new();
@@ -39,3 +50,14 @@ fn load_config_from_file(file_path: &String) -> Result<Config, Error> {
 }
 
 fn run_with_config(config: &Config) {}
+
+fn read_from_file(file_path: &String) -> Result<Vec<Record>, csv::Error> {
+    let path = Path::new(file_path);
+    let mut rdr = Reader::from_path(path)?;
+    let mut records: Vec<Record> = Vec::new();
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        records.push(record);
+    }
+    Ok(records)
+}
