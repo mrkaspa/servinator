91e7fc244796167f65846bfe028616158ad56039
Michel Perez <michel.ingesoft@gmail.com>
2017-08-10 19:57:57 -0500

    wip

--- src/req.rs
+++ src/req.rs
@@ -1,8 +1,8 @@
 use hyper::{Client, Method, Request};
-use hyper::header::{ContentLength, ContentType, Authorization};
+use hyper::header::{Authorization, ContentLength, ContentType};
 use tokio_core::reactor::Core;
 use serde_json;
-use structs::{Task, Config, Service};
+use structs::{Config, Service, Task};
 
 pub fn do_reqs(config: &Config, records: &Vec<Task>) -> Result<(), String> {
     println!("conf = {:?}", config);
@@ -15,15 +15,13 @@ pub fn do_reqs(config: &Config, records: &Vec<Task>) -> Result<(), String> {
             let mut req = Request::new(Method::Post, uri);
             req.headers_mut().set(ContentType::json());
             req.headers_mut().set(ContentLength(json.len() as u64));
-            req.headers_mut()
-                .set(Authorization(config.token.clone()));
+            req.headers_mut().set(Authorization(config.token.clone()));
             req.set_body(json);
             println!("req {:?}", req);
 
             let mut core = Core::new().map_err(|e| e.to_string())?;
             let client = Client::new(&core.handle());
-            let res = core.run(client.request(req))
-                .map_err(|e| e.to_string())?;
+            let res = core.run(client.request(req)).map_err(|e| e.to_string())?;
             println!("res {:?}", res);
             Ok(())
         }
